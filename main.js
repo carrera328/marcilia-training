c7347a2ab4dd07e0230730551ba8fd41e8adafab
Marcilia Alves <marcilliaalves@gmail.com>
2023-04-12 20:38:43 -0400

    creat a input for me

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const element = document.querySelector('#text-to-update');
-console.log('element', element);
+// const element = document.querySelector('#text-to-update');
+// console.log('element', element);
+
+
+
+
+function myFunction() {
+    document.getElementById("text-to-update").innerHTML = "Hello, my name is Dash Cat!";
+  }
